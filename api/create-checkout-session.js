41bc9a5aca7ea9f5bf9452f46f690374dce6539b
Mike Jerome <m@j4e.name>
2020-09-05 21:32:50 +0100

    Basic stripe integration

---
+++ api/create-checkout-session.js
@@ -0,0 +1,23 @@
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+module.exports = async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "T-shirt",
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "https://example.com/success",
+    cancel_url: "https://example.com/cancel",
+  });
+
+  res.json({ id: session.id });
+};
