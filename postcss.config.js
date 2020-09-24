2fa23dad1c8ef23994cebbce5b97fb9d8cca3afa
Mike Jerome <m@j4e.name>
2020-09-24 22:50:54 +0100

    Tailwind content

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   plugins: [
     require("tailwindcss")("tailwind.config.js"),
-    require("@fullhuman/postcss-purgecss")({
-      content: ["./tailwind.njk"],
-      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    }),
-    require("postcss-clean"),
+    // require("@fullhuman/postcss-purgecss")({
+    //   content: ["*.*"],
+    //   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    // }),
+    // require("postcss-clean"),
   ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      maxHeight: {
+        halfscreen: "50vh",
+      },
+      width: {
+        halfscreen: "50vw",
+      },
+      colors: {
+        cultured: "#f1f1f1",
+        spacecadet: "#323158",
+      },
+    },
   },
   variants: {},
   plugins: [require("@tailwindcss/typography")],
