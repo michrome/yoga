68fb9829efc3fbb58a039fbcedb7c32acee29a38
Mike Jerome <m@j4e.name>
2020-09-25 15:12:39 +0100

    Try cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   plugins: [
     require("tailwindcss")("tailwind.config.js"),
-    // require("@fullhuman/postcss-purgecss")({
-    //   content: ["*.*"],
-    //   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    // }),
-    // require("postcss-clean"),
+    require("@fullhuman/postcss-purgecss")({
+      content: ["*.*"],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+    require("cssnano")({
+      preset: "default",
+    }),
   ],
 };
