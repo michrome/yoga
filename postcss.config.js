588a4d5126a464dbf5a9e316bab4f328c7d5166a
Mike Jerome <m@j4e.name>
2020-10-04 22:06:13 +0100

    Move layout to an include

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require("tailwindcss")("tailwind.config.js"),
     require("@fullhuman/postcss-purgecss")({
-      content: ["*.*"],
+      content: ["*.njk", "./_includes/*.njk"],
       defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
     }),
     require("cssnano")({
